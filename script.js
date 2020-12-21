07905186745e89c35aba5cb290ce2aacf3b5617c
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-20 19:15:27 -0800

    working icon and description

--- script.js
+++ script.js
@@ -4,33 +4,30 @@
 //     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + apiKey;
 
 
-// Wait for elements to finish loading by placing code in $(function(){})
+
 $(function () {
-    // Replace API_KEY with your api key for OpenWeatherAPI
-    var apiKey = "c5b501b47a94dbd44148a427c20e831f";
+    
+    var apiKey = "e06e90d8fc4837e944f6dcccda5f3e8a";
     $(document).on("click", ".city", function () {
-      // get the name of the city using the data-city attribute of the clicked
-      // element
+      
       var city = $(this).attr("data-city");
-      // construct a url to search OpenWeatherAPI for the current weather in the
-      // city
+      
       var queryUrl =
         "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=imperial&appid=" +
         apiKey;
-      // send ajax request for current weather to OpenWeatherAPI
+      
       $.ajax({
         url: queryUrl,
         method: "GET",
       }).then(function (data) {
-        // log the data from the api to the console
+       
         console.log(data);
-        // set the text of the #city-name h2 element using the city name in the
-        // response
+        
         $("#city-name").text(data.name + " Weather");
-        // display the temperature, wind, and humidity in the elements found in
-        // the html. (set the text)
+        $("#icon").html("<img src='http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+        $("description").text(data.weather[0].description);
         $("#temp").text(data.main.temp + "°");
         $("#wind").text(data.wind.speed + " mph");
         $("#humidity").text(data.main.humidity + " %");
@@ -127,61 +124,3 @@ $(function () {
 
 
 
-
-
-// (function () {
-//     var apiKey = "c5b501b47a94dbd44148a427c20e831f";
-//     $(document).on("click", ".city", function() {
-
-//         var city = $(this).attr("data-city");
-//         var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearch+ "&units=imperial&appid=" +apiKey;
-
-//         $.ajax({
-//             url: queryUrl,
-//             method: "GET",
-//         }).then(function (data) {
-//             console.log(data);
-
-//             $("#city-name").text(data.name + " Weather");
-
-//             $("#temp").text(data.main.temp + "°");
-//             $("#wind").text(data.wind.speed + " mph");
-//             $("#humidity").text(data.main.humidity + " %");
-
-//             $("#search-input").val("");
-//         });
-
-//     });
-
-//     $("#search-form").on("submit", function (event) {
-//         event.preventDefault();
-
-//         var citySearch = $("#search-input").val().trim();
-//         //getWeather(citySearch);
-        
-//         if (citySearch === "") {
-//             return;
-//         }
-        
-        
-//         var citySearch = $("#search-input").val().trim();
-        
-//         $.ajax({
-//             url: queryURL,
-//             method: "GET",
-//         }).then(function (data) {
-//             console.log(data);
-
-        
-//             $("#city-name").text(data.name + " Weather");
-
-//             $("#temp").text(data.main.temp + "°");
-//             $("#wind").text(data.wind.speed + " mph");
-//             $("#humidity").text(data.main.humidity + " %");
-
-//             $("#search-input").val("");
-
-// });
-
-// })
-// })
