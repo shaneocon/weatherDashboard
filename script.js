5bde82b36d67c6bb7a5bd4d6c71e37150641b9e3
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-20 18:33:09 -0800

    added search button links

--- script.js
+++ script.js
@@ -3,36 +3,59 @@
 //     var citySearch = $("#search-input").val().trim();
 //     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + apiKey;
 
-
-$("#search-form").on("submit", function (event) {
-    event.preventDefault();
-
-    var citySearch = $("#search-input").val().trim();
-    //getWeather(citySearch);
-    
-    if (citySearch === "") {
-        return;
-    }
-    
+(function () {
     var apiKey = "e06e90d8fc4837e944f6dcccda5f3e8a";
-    var citySearch = $("#search-input").val().trim();
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + apiKey;
+    $(document).on("click", ".city", function() {
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (data) {
-        console.log(data);
+        var city = $(this).attr("data-city");
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearch+ "&units=imperial&appid=" +apiKey;
 
-    
-    $("#city-name").text(data.name + " Weather");
+        $.ajax({
+            url: queryUrl,
+            method: "GET",
+        }).then(function (data) {
+            console.log(data);
 
-    $("#temp").text(data.main.temp + "°");
-    $("#wind").text(data.wind.speed + " mph");
-    $("#humidity").text(data.main.humidity + " %");
+            $("#city-name").text(data.name + " Weather");
 
-    $("#search-input").val("");
+            $("#temp").text(data.main.temp + "°");
+            $("#wind").text(data.wind.speed + " mph");
+            $("#humidity").text(data.main.humidity + " %");
+
+            $("#search-input").val("");
+        });
+
+    });
+
+    $("#search-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var citySearch = $("#search-input").val().trim();
+        //getWeather(citySearch);
+        
+        if (citySearch === "") {
+            return;
+        }
+        
+        
+        var citySearch = $("#search-input").val().trim();
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (data) {
+            console.log(data);
+
+        
+            $("#city-name").text(data.name + " Weather");
+
+            $("#temp").text(data.main.temp + "°");
+            $("#wind").text(data.wind.speed + " mph");
+            $("#humidity").text(data.main.humidity + " %");
+
+            $("#search-input").val("");
 
 });
 
+})
 })
