34ee533bdec459ae3701b18eb5f99ea828bbbe05
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-20 22:18:54 -0800

    working 5 day forecast (no icons yet)

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $(function () {
         console.log(data);
         
         $("#city-name").text(data.name + " Weather");
-        $("#icon").html("<img src='http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+        $("#weather-icon").html("<img src='http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
         $("#description").text(data.weather[0].description);
         $("#temp").text(data.main.temp + "°");
         $("#wind").text(data.wind.speed + " mph");
@@ -58,7 +58,8 @@ $(function () {
         console.log(data);
         
         $("#city-name").text(data.name + " Weather");
-        
+        $("#weather-icon").html("<img src='http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+        $("#description").text(data.weather[0].description);
         $("#temp").text(data.main.temp + "°");
         $("#wind").text(data.wind.speed + " mph");
         $("#humidity").text(data.main.humidity + " %");
@@ -79,7 +80,7 @@ $(function () {
         }
 
         var queryUrl =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
         search +
         "&units=imperial&appid=" +
         apiKey;
@@ -90,23 +91,97 @@ $(function () {
         }).then(function (data) {
         
         console.log(data);
-        var dayOne = moment().format("M/D/YYYY");
-        var dayOneIcon = data.list[0].weather[0].icon;
-        var dayOneImg = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + dayOneIcon + ".png");
-        var dayOneTemp = data.main.temp + " °F";
-        var dayOneHumid = data.main.humidity + "%";
+         // --------- DISPLAY DAY ONE ------------- //
 
-        // DISPLAY DAY ONE
+        var dayOne = moment().format("M/D/YYYY");
+        // var dayOneIcon = data.weather[0].icon;
+        //var dayOneImg = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + dayOneIcon + ".png");
+        var dayOneTemp = data.list[0].main.temp + " °F";
+        var dayOneHumid = data.list[0].main.humidity + "%";
 
         var dayOneDateEl = $("<p>").text(dayOne);
-        var dayOneImgEl = $("<p>").text(dayOneIcon);
+        // var dayOneImgEl = $("<p>").html(dayOneIcon);
         var dayOneTempEl = $("<p>").text("Temp: " + dayOneTemp);
         var dayOneHumidEl = $("<p>").text("Humdity: " + dayOneHumid);
 
             $(".dayOne").append(dayOneDateEl);
-            $(".dayOne").append(dayOneImgEl);
+            // $(".dayOne").append(dayOneImgEl);
             $(".dayOne").append(dayOneTempEl);
             $(".dayOne").append(dayOneHumidEl);
+
+         // --------- DISPLAY DAY TWO ------------- //
+
+        var dayTwo = moment().add(1, "days").format("M/D/YYYY");
+        // var dayTwoIcon = data[8].weather[0].icon;
+        //var dayTwoImg = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + dayTwoIcon + ".png");
+        var dayTwoTemp = data.list[8].main.temp + " °F";
+        var dayTwoHumid = data.list[8].main.humidity + "%";
+
+        var dayTwoDateEl = $("<p>").text(dayTwo);
+        // var dayTwoImgEl = $("<p>").text(dayTwoIcon);
+        var dayTwoTempEl = $("<p>").text("Temp: " + dayTwoTemp);
+        var dayTwoHumidEl = $("<p>").text("Humdity: " + dayTwoHumid);
+
+            $(".dayTwo").append(dayTwoDateEl);
+            // $(".dayTwo").append(dayTwoImgEl);
+            $(".dayTwo").append(dayTwoTempEl);
+            $(".dayTwo").append(dayTwoHumidEl);
+
+        // --------- DISPLAY DAY THREE ------------- //
+
+        var dayThree = moment().add(2, "days").format("M/D/YYYY");
+        // var dayThreeIcon = data.list[16].weather[0].icon;
+        // var dayThreeImg = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + dayThreeIcon + ".png");
+        var dayThreeTemp = data.list[16].main.temp + " °F";
+        var dayThreeHumid = data.list[16].main.humidity + "%";
+
+        var dayThreeDateEl = $("<p>").text(dayThree);
+        // var dayThreeImgEl = $("<p>").text(dayOneIcon);
+        var dayThreeTempEl = $("<p>").text("Temp: " + dayThreeTemp);
+        var dayThreeHumidEl = $("<p>").text("Humdity: " + dayThreeHumid);
+
+            $(".dayThree").append(dayThreeDateEl);
+            // $(".dayThree").append(dayThreeImgEl);
+            $(".dayThree").append(dayThreeTempEl);
+            $(".dayThree").append(dayThreeHumidEl);
+
+        // --------- DISPLAY DAY FOUR ------------- //
+
+        var dayFour = moment().add(3, "days").format("M/D/YYYY");
+        // var dayFourIcon = data.list[24].weather[0].icon;
+        // var dayFoudImg = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + dayFourIcon + ".png");
+        var dayFourTemp = data.list[24].main.temp + " °F";
+        var dayFourHumid = data.list[24].main.humidity + "%";
+
+        var dayFourDateEl = $("<p>").text(dayFour);
+        // var dayFourImgEl = $("<p>").text(dayOneIcon);
+        var dayFourTempEl = $("<p>").text("Temp: " + dayFourTemp);
+        var dayFourHumidEl = $("<p>").text("Humdity: " + dayFourHumid);
+
+            $(".dayFour").append(dayFourDateEl);
+           //  $(".dayFour").append(dayFourImgEl);
+            $(".dayFour").append(dayFourTempEl);
+            $(".dayFour").append(dayFourHumidEl);
+
+        // --------- DISPLAY DAY FIVE ------------- //
+
+        var dayFive = moment().add(4, "days").format("M/D/YYYY");
+        // var dayFiveIcon = data.list[32].weather[0].icon;
+        // var dayFiveImg = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + dayFiveIcon + ".png");
+        var dayFiveTemp = data.list[32].main.temp + " °F";
+        var dayFiveHumid = data.list[32].main.humidity + "%";
+
+        var dayFiveDateEl = $("<p>").text(dayFive);
+        // var dayFiveImgEl = $("<p>").text(dayFiveIcon);
+        var dayFiveTempEl = $("<p>").text("Temp: " + dayFiveTemp);
+        var dayFiveHumidEl = $("<p>").text("Humdity: " + dayFiveHumid);
+
+            $(".dayFive").append(dayFiveDateEl);
+            // $(".dayFive").append(dayFiveImgEl);
+            $(".dayFive").append(dayFiveTempEl);
+            $(".dayFive").append(dayFiveHumidEl);
+
+        
             
 
 
