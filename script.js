47227882426e77ad9059e20cc4b8e1e07211d7f5
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-20 18:19:14 -0800

    queryURL link

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 // --------- VARIABLES ---------- //
-var apiKey = "e06e90d8fc4837e944f6dcccda5f3e8a";
-var city = $("#search-input").val().trim();
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+// var apiKey = "e06e90d8fc4837e944f6dcccda5f3e8a";
+//     var citySearch = $("#search-input").val().trim();
+//     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + apiKey;
 
 
-$("#search-form").submit(function(event) {
+$("#search-form").on("submit", function (event) {
     event.preventDefault();
 
-    var cityName = $("#search-input").val();
-    getWeather(cityName);
-})
+    var citySearch = $("#search-input").val().trim();
+    //getWeather(citySearch);
+    
+    if (citySearch === "") {
+        return;
+    }
+    
+    var apiKey = "e06e90d8fc4837e944f6dcccda5f3e8a";
+    var citySearch = $("#search-input").val().trim();
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + apiKey;
 
-function getWeather(city){
     $.ajax({
         url: queryURL,
         method: "GET",
-}).then(function (data) {
-    console.log(data);
+    }).then(function (data) {
+        console.log(data);
+
+    
+    $("#city-name").text(data.name + " Weather");
+
+    $("#temp").text(data.main.temp + "°");
+    $("#wind").text(data.wind.speed + " mph");
+    $("#humidity").text(data.main.humidity + " %");
+
+    $("#search-input").val("");
+
 });
 
-
-
-
-
-
-
-}
+})
