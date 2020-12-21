d8e6f4bfa9d8fecc04ff2fb931229cc0b8c17c63
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-20 17:08:09 -0800

    added queryURL and apiKey, getWeather fnc beginning

--- script.js
+++ script.js
@@ -1,185 +1,28 @@
+// --------- VARIABLES ---------- //
+var apiKey = "e06e90d8fc4837e944f6dcccda5f3e8a";
+var city = $("#search-input").val().trim();
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>Weather Button</title>
-//     <link
-//       rel="stylesheet"
-//       href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-//       integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-//       crossorigin="anonymous"
-//     />
-//   </head>
-//     <body>
-//         <div class="container pt-3 pl-3 bg-dark text-light">
-//             <h1 class="text-center">Current Weather</h1>
-//             <div class="row mt-5">
-//                 <div class="col-md-4">
-//                 <div class="card mb-3">
-//                     <div class="card-body">
-//                     <form id="search-form">
-//                         <div class="form-group">
-//                         <label for="search-input">Search for a city</label>
-//                         <div class="input-group">
-//                             <input
-//                             type="text"
-//                             class="form-control"
-//                             id="search-input"
-//                             placeholder="e.g. San Francisco"
-//                             />
-//                             <div class="input-group-append">
-//                             <button class="btn btn-primary" type="submit">
-//                                 Search
-//                             </button>
-//                             </div>
-//                         </div>
-//                         </div>
-//                     </form>
-//                     <h2 class="card-title h4">Cities</h2>
-//                     <h3 class="card-subtitle h6 font-weight-normal mb-3 text-muted">
-//                         Click on a city to view the weather
-//                     </h3>
-//                     <button
-//                         data-city="Austin"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         Austin
-//                     </button>
-//                     <button
-//                         data-city="Chicago"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         Chicago
-//                     </button>
-//                     <button
-//                         data-city="New York"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         New York
-//                     </button>
-//                     <button
-//                         data-city="Orlando"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         Orlando
-//                     </button>
-//                     <button
-//                         data-city="San Francisco"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         San Francisco
-//                     </button>
-//                     <button
-//                         data-city="Seattle"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         Seattle
-//                     </button>
-//                     <button
-//                         data-city="Denver"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         Denver
-//                     </button>
-//                     <button
-//                         data-city="Atlanta"
-//                         class="city btn btn-outline-primary btn-block"
-//                     >
-//                         Atlanta
-//                     </button>
-//                     <hr />
-//                     </div>
-//                 </div>
-//                 </div>
-//                 <div class="col-md-8">
-//                 <div class="card weather-card bg-primary text-light mb-3">
-//                     <div class="card-body">
-//                     <h2 id="city-name" class="card-title h4">Choose City</h2>
-//                     <hr class="border-top border-light" />
-//                     <div id="weather-icon"></div>
-//                     <p class="card-text">
-//                         Current Temp (F): <span id="temp">-</span>
-//                     </p>
-//                     <p class="card-text">Wind: <span id="wind">-</span></p>
-//                     <p class="card-text">Humidity: <span id="humidity">-</span></p>
-//                     </div>
-//                 </div>
-//                 <!--<div class="card weather-card text-light">>-->
-//                 <div class="row card-title" id="forecastHeader">
-//                     <h4>Five Day Forecast: </h4>
-//                 </div>
-//                 <div class="row" id=forcastCards></div>
-//                     <div class="col">
-//                         <div class="card text-white bg-primary mb-3">
-//                             <div class="dayOne">
-//                                 <p class="temp"></p>
-//                                 <p class="humidity"></p>
-//                             </div>
-//                         </div>
-//                     </div>
 
-//                     <div class="card text-white bg-primary mb-3" style="max-width: 9rem;">
-//                         <div class="dayTwo">
-//                             <p class="temp"></p>
-//                             <p class="humidity"></p>
-//                         </div>
-//                     </div>
-//                     <div class="card text-white bg-primary mb-3" style="max-width: 9rem;">
-//                         <div class="dayThree">
-//                             <p class="temp"></p>
-//                             <p class="humidity"></p>
-//                         </div>
-//                     </div>
-                    
-//                     <div class="card text-white bg-primary mb-3" style="max-width: 9rem;">
-//                         <div class="dayFour">
-//                             <p class="temp"></p>
-//                             <p class="humidity"></p>
-//                         </div>
-//                     </div>
-        
-//                     <div class="card text-white bg-primary mb-3" style="max-width: 9rem;">
-//                         <div class="dayFive">
-//                             <p class="temp"></p>
-//                             <p class="humidity"></p>
-//                         </div>
-//                     </div>
-        
-//                 </div>    
-//             </div>    
-//         </div>
+$("#search-form").submit(function(event) {
+    event.preventDefault();
 
-//         <script
-//         src="https://code.jquery.com/jquery-3.5.1.min.js"
-//         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-//         crossorigin="anonymous"
-//         ></script>
-//         <script src="script.js"></script>
-//     </body>
+    var cityName = $("#search-input").val();
+    getWeather(cityName);
+})
 
-// </html>
+function getWeather(city){
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+}).then(function (data) {
+    console.log(data);
+});
 
 
 
 
 
 
-// <!--
-// <div class="card-body">
-//     <h2 id="city-name" class="card-title h4">Choose City</h2>
-//     <hr class="border-top border-light" />
-//     <div id="weather-icon"></div>
-//     <p class="card-text">
-//       Current Temp (F): <span id="temp">-</span>
-//     </p>
-//     <p class="card-text">Wind: <span id="wind">-</span></p>
-//     <p class="card-text">Humidity: <span id="humidity">-</span></p>
-//   </div>
-// </div>>
 
-// -->
-
-
-
+}
